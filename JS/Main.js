a1339c700df00ffc97ad7d17f09eeb6ce693add9
Ahmedsakr-developer <ahmed20208015@fci.helwan.edu.eg>
2021-01-19 00:59:56 +0200

    edit

--- JS/Main.js
+++ JS/Main.js
@@ -1,39 +1,39 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        autoplay:true,
-    autoplayTimeout:3000,
-        responsive:{
-            0:{
-                items:1
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:2
+            600: {
+                items: 2
             },
-            1000:{
-                items:3
+            1000: {
+                items: 3
             }
         }
     });
     $('.ImgSlider').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        autoplay:true,
-    autoplayTimeout:3000,
-        responsive:{
-            0:{
-                items:1
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:3
+            600: {
+                items: 3
             },
-            1000:{
-                items:4
+            1000: {
+                items: 4
             }
         }
     });
-  });
+});
 
