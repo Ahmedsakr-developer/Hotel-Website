8ff6d4b50c0545b2126135551e18e846c734269d
Elsayedmostafa574 <alsayedb574@gmail.com>
2021-01-18 14:04:35 +0200

    elsayed

---
+++ JS/Main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+    $('.ImgSlider').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+  });
+
